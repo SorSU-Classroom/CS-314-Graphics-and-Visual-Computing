e80e55b97ee00884db069d9ff1a591db94106a89
Jarrian <glorianagojar@gmail.com>
2024-08-26 00:35:13 +0800

    feat(L2.4): Lesson with PDF

--- 2. Image Basics/2.4. Color Models/2.4. Color Models.py
+++ 2. Image Basics/2.4. Color Models/2.4. Color Models.py
@@ -3,8 +3,8 @@ Color Models
 
 A color model is a mathematical model describing the way colors can be represented as
 tuples of numbers. It is used to describe the way colors are represented in an image.
-There are several color models used in image processing, such as RGB, HSV, YUV(YIQ),
-and CIE L*a*b.
+There are several color models used in image processing, such as RGB, HSV, YUV and
+CIE L*a*b.
 
 These color models are used to represent colors in different ways. For example, the RGB
 color model is used to represent colors in terms of red, green, and blue components.
@@ -21,6 +21,8 @@ color segmentation, and the CIE L*a*b color model is used for color correction.
 In this section, we will learn about the RGB color model, which is the most commonly
 used color model in image processing. We will also learn how to convert images from one
 color model to another using OpenCV and Matplotlib.
+
+@see: https://en.wikipedia.org/wiki/Color_model
 '''
 
 # Initial Setup
@@ -30,6 +32,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 image_path = '../../assets/images/parrot.jpg'
+# image_path = '../../assets/images/rgb_volcano_1.jpeg'
 
 # Reading an image from the file using OpenCV
 original_image = cv2.imread(image_path)
@@ -43,10 +46,21 @@ average of the three color channels is calculated using the formula:
     Gray = (R + G + B) / 3
 
 where R, G, and B are the red, green, and blue color channels of the image, respectively.
+
+@see: https://en.wikipedia.org/wiki/Grayscale
 '''
+# Read Image in Grayscale Mode
 grayscale_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-# Convert color from BGR to RGB to correctly display the image
+'''
+Convert from BGR to RGB
+
+OpenCV reads images in the BGR (Blue-Green-Red) color space by default. However,
+Matplotlib expects images to be in the RGB (Red-Green-Blue) color space. Therefore, we
+need to convert the image from the BGR color space to the RGB color space before
+displaying it using Matplotlib.
+'''
+# Convert color from BGR to RGB
 original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 grayscale_image = cv2.cvtColor(grayscale_image, cv2.COLOR_BGR2RGB)
 
@@ -81,7 +95,7 @@ The RGB color model is used to represent colors in digital images, computer grap
 and television screens. It is the most commonly used color model in image processing
 and computer vision.
 
-Example of RGB Color Model
+Example of RGB Color Model:
 - Red: (255, 0, 0)
 - Green: (0, 255, 0)
 - Blue: (0, 0, 255)
@@ -91,41 +105,49 @@ Example of RGB Color Model
 - Cyan: (0, 255, 255)
 - Magenta: (255, 0, 255)
 - Gray: (128, 128, 128)
+
+@see: https://en.wikipedia.org/wiki/RGB_color_model
 '''
 
-# Splitting the image into its three color channels
+# Splitting the RGB image into its three color channels
 channel_b, channel_g, channel_r = cv2.split(original_image)
 
 # Displaying the three color channels
 print("Displaying the RGB Color Model and its channels")
 plt.figure('RGB Color Model')
 
-plt.subplot(2, 3, 1)
+# Display the Red Channel
+plt.subplot(3, 3, 1)
 plt.imshow(channel_r)
 plt.title('Red Channel')
 plt.axis('off')
 
-plt.subplot(2, 3, 2)
+# Display the Green Channel
+plt.subplot(3, 3, 2)
 plt.imshow(channel_g)
 plt.title('Green Channel')
 plt.axis('off')
 
-plt.subplot(2, 3, 3)
+# Display the Blue Channel
+plt.subplot(3, 3, 3)
 plt.imshow(channel_b)
 plt.title('Blue Channel')
 plt.axis('off')
 
-plt.subplot(2, 3, 4)
+# Display the Red Channel in Grayscale
+plt.subplot(3, 3, 4)
 plt.imshow(channel_r, cmap='gray')
 plt.title('Red Channel (BW)')
 plt.axis('off')
 
-plt.subplot(2, 3, 5)
+# Display the Green Channel in Grayscale
+plt.subplot(3, 3, 5)
 plt.imshow(channel_g, cmap='gray')
 plt.title('Green Channel (BW)')
 plt.axis('off')
 
-plt.subplot(2, 3, 6)
+# Display the Blue Channel in Grayscale
+plt.subplot(3, 3, 6)
 plt.imshow(channel_b, cmap='gray')
 plt.title('Blue Channel (BW)')
 plt.axis('off')
@@ -157,6 +179,14 @@ Example of HSV Color Model
 - Blue: (240, 100%, 100%)
 - White: (0, 0%, 100%)
 - Black: (0, 0%, 0%)
+
+In OpenCV, the maximum value for the hue component is 180, the maximum value for the
+saturation and value components is 255. This is because OpenCV uses the 8-bit unsigned
+integer data type to represent the color components which have a range of 0 to 255. So
+the hue component is scaled down to the range of 0 to 180 degrees in OpenCV or by
+dividing the original 0-360 degrees hue value by 2.
+
+@see: https://en.wikipedia.org/wiki/HSL_and_HSV
 '''
 
 '''
@@ -170,6 +200,8 @@ image is cv2.COLOR_RGB2HSV.
 The cv2.cvtColor() function returns the HSV image as a NumPy array. We can display the
 HSV image using Matplotlib by converting it to the RGB color space using the
 cv2.cvtColor() function with the cv2.COLOR_HSV2RGB color space conversion code.
+
+@see: https://docs.opencv.org/4.x/d8/d01/group__imgproc__color__conversions.html
 '''
 
 # Converting the RGB image to the HSV image
@@ -221,6 +253,9 @@ We can mask red pixels in an image by converting the image to the HSV color spac
 applying a mask to the hue channel. The mask is created by defining the lower and upper
 bounds of the red color range in the hue channel. The mask is then applied to the hue
 and saturation channels to remove the red pixels from the image.
+
+@see: https://en.wikipedia.org/wiki/Color_mask
+@see: https://docs.opencv.org/4.x/d2/de8/group__core__array.html#ga48af0ab51e36436c5d04340e036ce981
 '''
 
 # Convert image to HSV
@@ -232,12 +267,12 @@ channel_h, channel_s, channel_v = cv2.split(hsv_image)
 # Define the lower and upper bounds of the red color range
 
 # Mask 1 are the red pixels with hue values between 0 and 10
-lower_red = np.array([0, 100, 40])
+lower_red = np.array([0, 100, 100])
 upper_red = np.array([10, 255, 255])
 mask_red_1 = cv2.inRange(hsv_image, lower_red, upper_red)
 
 # Mask 2 are the red pixels with hue values between 160 and 180
-lower_red = np.array([160, 100, 40])
+lower_red = np.array([160, 100, 100])
 upper_red = np.array([180, 255, 255])
 mask_red_2 = cv2.inRange(hsv_image, lower_red, upper_red)
 
@@ -256,7 +291,7 @@ modified_RGB_image = cv2.cvtColor(modified_HSV_image, cv2.COLOR_HSV2RGB)
 
 # Display the modified image
 print("Displaying the modified image with masked red pixels")
-plt.figure('Modified Image with Masked Red Pixels')
+plt.figure('Image with Masked Red Pixels')
 
 plt.subplot(1, 3, 1)
 plt.imshow(original_image)
@@ -270,7 +305,7 @@ plt.axis('off')
 
 plt.subplot(1, 3, 3)
 plt.imshow(modified_RGB_image)
-plt.title('Masked Red Pixels from Original Image')
+plt.title('Masked Red Pixels')
 plt.axis('off')
 
 plt.show()
@@ -282,6 +317,8 @@ We can mask green pixels in an image by converting the image to the HSV color sp
 applying a mask to the hue channel. The mask is created by defining the lower and upper
 bounds of the green color range in the hue channel. The mask is then applied to the hue
 and saturation channels to remove the green pixels from the image.
+
+@see: https://en.wikipedia.org/wiki/Color_mask
 '''
 
 # Convert image to HSV
@@ -307,7 +344,7 @@ modified_RGB_image = cv2.cvtColor(modified_HSV_image, cv2.COLOR_HSV2RGB)
 
 # Display the modified image
 print("Displaying the modified image with masked green pixels")
-plt.figure('Modified Image with Masked Green Pixels')
+plt.figure('Image with Masked Green Pixels')
 
 plt.subplot(1, 3, 1)
 plt.imshow(original_image)
@@ -321,7 +358,7 @@ plt.axis('off')
 
 plt.subplot(1, 3, 3)
 plt.imshow(modified_RGB_image)
-plt.title('Masked Green Pixels from Original Image')
+plt.title('Masked Green Pixels')
 plt.axis('off')
 
 plt.show()
@@ -333,6 +370,8 @@ We can mask blue pixels in an image by converting the image to the HSV color spa
 applying a mask to the hue channel. The mask is created by defining the lower and upper
 bounds of the blue color range in the hue channel. The mask is then applied to the hue
 and saturation channels to remove the blue pixels from the image.
+
+@see: https://en.wikipedia.org/wiki/Color_mask
 '''
 
 # Convert image to HSV
@@ -358,7 +397,7 @@ modified_RGB_image = cv2.cvtColor(modified_HSV_image, cv2.COLOR_HSV2RGB)
 
 # Display the modified image
 print("Displaying the modified image with masked blue pixels")
-plt.figure('Modified Image with Masked Blue Pixels')
+plt.figure('Image with Masked Blue Pixels')
 
 plt.subplot(1, 3, 1)
 plt.imshow(original_image)
@@ -372,7 +411,7 @@ plt.axis('off')
 
 plt.subplot(1, 3, 3)
 plt.imshow(modified_RGB_image)
-plt.title('Masked Blue Pixels from Original Image')
+plt.title('Masked Blue Pixels')
 plt.axis('off')
 
 plt.show()
@@ -393,6 +432,8 @@ Note: The hue channel in the HSV color space is represented as an angle between 
 360 degrees. Therefore, when adding or subtracting a constant value from the hue
 channel, we need to ensure that the resulting hue values are within the range of 0 to
 360 degrees.
+
+@see: https://en.wikipedia.org/wiki/Hue
 '''
 
 # Convert image to HSV
@@ -413,7 +454,7 @@ modified_RGB_image = cv2.cvtColor(modified_HSV_image, cv2.COLOR_HSV2RGB)
 
 # Display the modified image
 print("Displaying the modified image with shifted hue")
-plt.figure('Modified Image with Shifted Hue')
+plt.figure('Image with Shifted Hue')
 
 plt.subplot(1, 2, 1)
 plt.imshow(original_image)
@@ -422,7 +463,7 @@ plt.axis('off')
 
 plt.subplot(1, 2, 2)
 plt.imshow(modified_RGB_image)
-plt.title('Modified Image with Shifted Hue')
+plt.title('Image with Shifted Hue')
 plt.axis('off')
 
 plt.show()
@@ -434,6 +475,8 @@ We can selectively modify the color of an image by converting the image to the H
 space and applying a mask to the hue channel. The mask is created by defining the lower
 and upper bounds of the color range in the hue channel. The mask is then applied to the
 hue channel to modify the color of the pixels within the specified range.
+
+@see: https://en.wikipedia.org/wiki/Color_mask
 '''
 
 # Convert image to HSV
@@ -445,12 +488,12 @@ channel_h, channel_s, channel_v = cv2.split(hsv_image)
 # Define the lower and upper bounds of the color range
 
 # Mask 1 are the red pixels with hue values between 0 and 10
-lower_color = np.array([0, 100, 40])
+lower_color = np.array([0, 100, 100])
 upper_color = np.array([10, 255, 255])
 mask_color_1 = cv2.inRange(hsv_image, lower_color, upper_color)
 
 # Mask 2 are the red pixels with hue values between 160 and 180
-lower_color = np.array([160, 100, 40])
+lower_color = np.array([160, 100, 100])
 upper_color = np.array([180, 255, 255])
 mask_color_2 = cv2.inRange(hsv_image, lower_color, upper_color)
 
@@ -458,7 +501,7 @@ mask_color_2 = cv2.inRange(hsv_image, lower_color, upper_color)
 mask_color = cv2.bitwise_or(mask_color_1, mask_color_2)
 
 # Modify the hue channel by adding a constant value to the pixels within the color range
-hue_shift = 20
+hue_shift = 15
 channel_h[mask_color > 0] = (channel_h[mask_color > 0] + hue_shift) % 180
 
 # Combine the modified hue and saturation channels with the original value channel
@@ -469,7 +512,7 @@ modified_RGB_image = cv2.cvtColor(modified_HSV_image, cv2.COLOR_HSV2RGB)
 
 # Display the modified image
 print("Displaying the modified image with selective color modification")
-plt.figure('Modified Image with Selective Color Modification')
+plt.figure('Image with Color Changed')
 
 plt.subplot(1, 3, 1)
 plt.imshow(original_image)
@@ -483,7 +526,142 @@ plt.axis('off')
 
 plt.subplot(1, 3, 3)
 plt.imshow(modified_RGB_image)
-plt.title('Modified Image with Selective Color Modification')
+plt.title('Image with Selective Color Modification')
+plt.axis('off')
+
+plt.show()
+
+'''
+YUV Color Family
+
+The YUV color family is a color model used in video compression and television
+broadcasting.
+
+It consists of three components:
+- Y (luminance)
+    - Represents the brightness of the color
+    - Similar to the grayscale image
+- U (chrominance, primarily blue-yellow)
+    - U = B - Y (chrominance, primarily blue-yellow)
+- V (chrominance, primarily red-green)
+    - V = R - Y (chrominance, primarily red-green)
+
+The Y component represents the brightness of the color, while the U and V components represent
+the color information.
+
+The YUV color family is used to represent colors in a way that is more efficient for
+video compression than the RGB color model. It separates the brightness and color
+information of the image, allowing for better compression and transmission of video
+data.
+
+@see: https://en.wikipedia.org/wiki/YUV
+'''
+
+# Convert image to YUV
+yuv_image = cv2.cvtColor(original_image, cv2.COLOR_RGB2YUV)
+
+# Divide the YUV image into its three color channels
+channel_y, channel_u, channel_v = cv2.split(yuv_image)
+
+# Displaying the three color channels
+print("Displaying the YUV Color Model and its channels")
+plt.figure('YUV Color Model')
+
+plt.subplot(2, 3, 1)
+plt.imshow(channel_y)
+plt.title('Y Channel')
+plt.axis('off')
+
+plt.subplot(2, 3, 2)
+plt.imshow(channel_u)
+plt.title('U Channel')
+plt.axis('off')
+
+plt.subplot(2, 3, 3)
+plt.imshow(channel_v)
+plt.title('V Channel')
+plt.axis('off')
+
+plt.subplot(2, 3, 4)
+plt.imshow(channel_y, cmap='gray')
+plt.title('Y Channel (BW)')
+plt.axis('off')
+
+plt.subplot(2, 3, 5)
+plt.imshow(channel_u, cmap='gray')
+plt.title('U Channel (BW)')
+plt.axis('off')
+
+plt.subplot(2, 3, 6)
+plt.imshow(channel_v, cmap='gray')
+plt.title('V Channel (BW)')
+plt.axis('off')
+
+plt.show()
+
+'''
+CIE L*a*b* Color Model
+
+The CIE L*a*b* color model is a color model used in color science and color
+management. CIE stands for the International Commission on Illumination or
+Commission Internationale de l'Eclairage in French, an organization that
+develops standards for color science.
+
+The CIE L*a*b* color model consists of three components:
+- L* (lightness)
+    - Represents the brightness of the color
+- a* (green-red)
+    - a* = (green - red) component
+- b* (blue-yellow)
+    - b* = (blue - yellow) component
+    
+The L* component represents the lightness of the color, while the a* and b*
+components represent the color information. The CIE L*a*b* color model is used
+to represent colors in a way that is more perceptually uniform than the RGB color
+model. It is often used in color science, color management, and color correction
+applications.
+
+@see: https://en.wikipedia.org/wiki/CIELAB_color_space
+'''
+
+# Convert image to CIE L*a*b*
+lab_image = cv2.cvtColor(original_image, cv2.COLOR_RGB2LAB)
+
+# Divide the CIE L*a*b* image into its three color channels
+channel_l, channel_a, channel_b = cv2.split(lab_image)
+
+# Displaying the three color channels
+print("Displaying the CIE L*a*b* Color Model and its channels")
+plt.figure('CIE L*a*b* Color Model')
+
+plt.subplot(2, 3, 1)
+plt.imshow(channel_l)
+plt.title('L* Channel')
+plt.axis('off')
+
+plt.subplot(2, 3, 2)
+plt.imshow(channel_a)
+plt.title('a* Channel')
+plt.axis('off')
+
+plt.subplot(2, 3, 3)
+plt.imshow(channel_b)
+plt.title('b* Channel')
+plt.axis('off')
+
+plt.subplot(2, 3, 4)
+plt.imshow(channel_l, cmap='gray')
+plt.title('L* Channel (BW)')
+plt.axis('off')
+
+plt.subplot(2, 3, 5)
+plt.imshow(channel_a, cmap='gray')
+plt.title('a* Channel (BW)')
+plt.axis('off')
+
+plt.subplot(2, 3, 6)
+plt.imshow(channel_b, cmap='gray')
+plt.title('b* Channel (BW)')
 plt.axis('off')
 
 plt.show()
